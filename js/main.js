df07d4116864c5179ca4a21d6ec85ae4b9c002a6
Thiwat Rongsirigul <thiwat9@hotmail.com>
2013-11-27 15:36:11 +0700

    preparing to talk with php code

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 function query(option) {
 	return $.ajax({
 		url: 'php/query.php',
-		data: { name: option.name, grade: option.grade, appearance: option.appearance, price: option.price }
+		data: { prod_name: option.prod_name, price_from: option.price_from, price_to: option.price_to, grade: option.grade, series: series, brand: brand }
 	})
 }
 
 $(function() {
 	$('#search').click(function() {
+		var prod_name = $('#search_name').val();
+		
+		var price = $('#price_select').find(":selected").val().split('-');
+		var price_from = price[0];
+		var price_to = price[1];
 
-		var name = $('#search_name').val();
 		var grade = $('#grade').val();
 		var appearance = $('#ep').val();
 		var brand = $('#brand').val();
-		var price = $('#price_select').find(":selected").val();
 
-		var promise = query({ name: name, grade: grade, appearance: appearance, price: price });
+		var promise = query({ prod_name: prod_name, price_from: price_from, price_to: price_to, grade: grade, series: series, brand: brand });
 	})
 })
--- php/gunpla_query.php
+++ php/gunpla_query.php
@@ -1,25 +1,26 @@
 <?php
 $db = mysqli_connect("localhost", "root", "", "danbai_shop");
 
+$prod_name_string = '%'.$_GET['prod_name'].'%'
 $statement = mysqli_prepare($db,
 	"SELECT *
-	FROM danbai_shop.gunpla
-	WHERE danbai_shop.gunpla.price BETWEEN ? AND ?
-	AND danbai_shop.gunpla.prod_name LIKE (?)
-	AND danbai_shop.gunpla.grade IN (?)
-	AND danbai_shop.gunpla.prod_id
+	FROM `danbai_shop`.`gunpla`
+	WHERE `danbai_shop`.`gunpla`.`price` BETWEEN ? AND ?
+	AND `danbai_shop`.`gunpla`.`prod_name` LIKE (?)
+	AND `danbai_shop`.`gunpla`.`grade` IN (?)
+	AND `danbai_shop`.`gunpla`.`prod_id`
 	IN (
-    	SELECT gunpla.prod_id
+    	SELECT `gunpla`.`prod_id`
     	FROM
-    	danbai_shop.series 
-    	INNER JOIN danbai_shop.appeared_in
-    	ON danbai_shop.series.series_id = danbai_shop.appeared_in.series_id
-        	INNER JOIN danbai_shop.gunpla
-        	ON danbai_shop.appeared_in.prod_id = danbai_shop.gunpla.prod_id
-            	INNER JOIN danbai_shop.brand
-            	ON danbai_shop.brand.brand_id = danbai_shop.gunpla.brand_id
-    	WHERE series_name IN (?)
-    	AND brand_name IN (?)
+    	`danbai_shop`.`series` 
+    	INNER JOIN `danbai_shop`.`appeared_in`
+    	ON `danbai_shop`.`series`.`series_id` = `danbai_shop`.`appeared_in`.`series_id`
+        	INNER JOIN `danbai_shop`.`gunpla`
+        	ON `danbai_shop`.`appeared_in`.`prod_id` = `danbai_shop`.`gunpla`.`prod_id`
+            	INNER JOIN `danbai_shop`.`brand`
+            	ON `danbai_shop`.`brand`.`brand_id` = `danbai_shop`.`gunpla`.`brand_id`
+    	WHERE `series_name` IN (?)
+    	AND `brand_name` IN (?)
 	)");
 
 echo mysqli_error($db);
@@ -27,7 +28,7 @@ echo mysqli_error($db);
 // i = int
 // s = string
 // d = double
-mysqli_stmt_bind_param($statement, 'iissss', $_GET['price_from'], $_GET['price_to'], $_GET['prod_name'], $_GET['grade'], $_GET['series'], $_GET['brand']);
+mysqli_stmt_bind_param($statement, 'iissss', $_GET['price_from'], $_GET['price_to'], $prod_name_string, $_GET['grade'], $_GET['series'], $_GET['brand']);
 mysqli_stmt_execute($statement);
 mysqli_stmt_bind_result($statement, $prod_id, $prod_name, $grade, $brand_name, $price);
 
@@ -46,14 +47,14 @@ while (mysqli_stmt_fetch($statement)) {
 
 for ($i=0; $i < count($product); $i++) {
     $statement = mysqli_prepare($db,
-	"SELECT gunpla.prod_id, gunpla.prod_name, series.series_name
+	"SELECT `gunpla`.`prod_id`, `gunpla`.`prod_name`, `series`.`series_name`
 	FROM
-	danbai_shop.series 
-    	INNER JOIN danbai_shop.appeared_in
-    	ON danbai_shop.series.series_id = danbai_shop.appeared_in.series_id
-        	INNER JOIN danbai_shop.gunpla
-        	ON danbai_shop.appeared_in.prod_id = danbai_shop.gunpla.prod_id
-	WHERE gunpla.prod_id = ?");
+	`danbai_shop`.`series` 
+    	INNER JOIN `danbai_shop`.`appeared_in`
+    	ON `danbai_shop`.`series`.`series_id` = `danbai_shop`.`appeared_in`.`series_id`
+        	INNER JOIN `danbai_shop`.`gunpla`
+        	ON `danbai_shop`.`appeared_in`.`prod_id` = `danbai_shop`.`gunpla`.`prod_id`
+	WHERE `gunpla`.`prod_id` = ?");
 
 	mysqli_stmt_bind_param($statement, 'i', $product[$i]['prod_id']);
 	mysqli_stmt_execute($statement);
