b8c3dfbd24cb3e7f8041ca0fd5aa484e8f2dbf25
DeViL-mAy-CrY <ken42298@gmail.com>
2013-11-28 15:14:03 +0700

    main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
+function query(option) {
+	return $.ajax({
+		url: 'php/query.php',
+		data: { name: option.name, grade: option.grade, appearance: option.appearance, price: option.price }
+	})
+}
+
 $(function() {
 	$('#search').click(function() {
 
 		var name = $('#search_name').val();
-		var grade = $('#select_grade').find(":selected").val();
-		var appearance = $('#appear_in').find(":selected").val();
-		var price = $('#price_range').find(":selected").val();
+		var grade = $('#grade').val();
+		var appearance = $('#ep').val();
+		var brand = $('#brand').val();
+		var price = $('#price_select').find(":selected").val();
 
 		var promise = query({ name: name, grade: grade, appearance: appearance, price: price });
 	})
